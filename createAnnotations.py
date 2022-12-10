23df470c48a6f630d36d7536e34b43a3d84d8a88
Noah Pritchard <noahpritchard9@gwu.edu>
2022-12-10 16:34:24 -0500

    updated annotations

--- createAnnotations.py
+++ createAnnotations.py
@@ -1,29 +1,22 @@
 import os
 from scipy.io import loadmat
-import csv
-import pandas
 import numpy as np
 
 dir = os.getcwd() + "/dataset/Airplanes_Side_2"
 
 index = 0
 with open(os.getcwd() + "/dataset/airplanes.csv", "w") as csv_file:
-    writer = csv.writer(csv_file)
     for filename in os.listdir(dir):
         index += 1
         try:
             mat = loadmat(dir + "/" + filename)
-            bb_list: list[list[str]] = [
-                [element for element in upperElement]
-                for upperElement in mat["box_coord"]
-            ]
+            coords = [x for x in mat["box_coord"][0]]
             annotationIndex = filename.split("_")[1].split(".")[0]
             annotationTitle = f"image_{annotationIndex}.jpg"
-            bb_list[0].append(annotationTitle)
-            vals = np.asarray(bb_list)
-            pandas.DataFrame(vals).to_csv()
-
-            writer.writerow(vals)
+            coords.append(annotationTitle)
+            vals = np.asarray(coords)
+            line = f"{vals[0]}, {vals[1]}, {vals[2]}, {vals[3]}, {vals[4]}\n"
+            csv_file.write(line)
 
         except:
             print("no file for " + dir + "/" + filename)
--- train.py
+++ train.py
@@ -3,7 +3,8 @@ import tensorflow as tf
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 import numpy as np
-import cv2  # not working
+
+# import cv2  # not working
 import os
 
 print("loading dataset...")
@@ -14,7 +15,7 @@ targets = []
 filenames = []
 
 for row in rows:
-    print(row)
+    print(row[0])
     # row = row.split(",")
     (startX, startY, endX, endY, filename) = row
     imagePath = os.path.sep.join([config.IMAGES_PATH, filename])
