13e7274aa4d73d6f691c9f40d34eded9eae6ad9c
Noah Pritchard <noahpritchard9@gwu.edu>
2022-12-10 19:29:27 -0500

    documentation for training

--- train.py
+++ train.py
@@ -4,47 +4,56 @@ from keras.applications import VGG16
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 import numpy as np
-
 import cv2
 import os
 
 print("loading dataset...")
 rows = open(config.ANNOTATIONS_PATH).read().strip().split("\n")
 
-data = []
-targets = []
+images = []
+coords = []
 filenames = []
 
 for row in rows:
+    # for each row, parse the csv input
+    # and get the coords/matching image
     row = row.split(",")
     (startX, startY, endX, endY, filename) = row
     imagePath = os.path.sep.join([config.IMAGES_PATH, filename.strip()])
     image = cv2.imread(imagePath)
+
+    # scale bounding box coords to the image
     (h, w) = image.shape[:2]
     startX = float(startX) / w
     startY = float(startY) / h
     endX = float(endX) / w
     endY = float(endY) / h
+
+    # load each image through keras and add image/coords/filename to arrays
     image = tf.keras.preprocessing.image.load_img(imagePath, target_size=(224, 224))
     image = tf.keras.preprocessing.image.img_to_array(image)
 
-    data.append(image)
-    targets.append((startX, startY, endX, endY))
+    images.append(image)
+    coords.append((startX, startY, endX, endY))
     filenames.append(filename)
 
-data = np.array(data, dtype="float32") / 255.0
-targets = np.array(targets, dtype="float32")
+# scale the images by 255 (for pixels), leave coords untouched
+images = np.array(images, dtype="float32") / 255.0
+coords = np.array(coords, dtype="float32")
 
-split = train_test_split(data, targets, filenames, test_size=0.10, random_state=42)
+# create training and testing splits for our model
+split = train_test_split(images, coords, filenames, test_size=0.10, random_state=42)
 (trainImages, testImages) = split[:2]
 (trainTargets, testTargets) = split[2:4]
 (trainFilenames, testFilenames) = split[4:]
 
+# save the test filenames into our output for later use
 print("saving testing filenames...")
 f = open(config.TEST_FILENAMES, "w")
 f.write("\n".join(testFilenames))
 f.close()
 
+# VGG16 is a premade model from keras that can be used for image classification
 vgg = VGG16(
     weights="imagenet",
     include_top=False,
@@ -55,6 +64,7 @@ vgg.trainable = False
 
 flatten = vgg.output
 
+# Flatten our data and add dense layers
 flatten = tf.keras.layers.Flatten()(flatten)
 
 bboxHead = tf.keras.layers.Dense(128, activation="relu")(flatten)
@@ -64,10 +74,12 @@ bboxHead = tf.keras.layers.Dense(4, activation="sigmoid")(bboxHead)
 
 model = tf.keras.models.Model(inputs=vgg.input, outputs=bboxHead)  # not sure ab this
 
+# Add an optimizer (Adam) to speed up our results
 opt = tf.keras.optimizers.Adam(learning_rate=config.INIT_LR)
 model.compile(loss="mse", optimizer=opt)
 print(model.summary())
 
+# Train the model
 print("training bounding box regressor...")
 H = model.fit(
     trainImages,
@@ -78,6 +90,7 @@ H = model.fit(
     verbose="auto",
 )
 
+# save the model to our output and plot the training data
 print("saving object detector model...")
 model.save(config.MODEL_PATH, save_format="h5")
 
@@ -87,7 +100,7 @@ plt.figure()
 plt.plot(np.arange(0, N), H.history["loss"], label="train_loss")
 plt.plot(np.arange(0, N), H.history["val_loss"], label="val_loss")
 plt.title("Bounding box regression loss on training set")
-plt.xlabel("Epoch #")
+plt.xlabel("Epoch")
 plt.ylabel("Loss")
 plt.legend(loc="lower left")
 plt.savefig(config.PLOT_PATH)
